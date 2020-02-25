4de5054ff0342381d009285cb384cd60ac3b93ef
brandenlacour <brandenlacour@gmail.com>
2020-02-25 12:04:40 -0600

    auth works

--- resources/users.py
+++ resources/users.py
@@ -25,6 +25,34 @@ def register():
         created_user = models.User.create(**payload)
         created_user.password = generate_password_hash(payload['password'])
         created_user.save()
+        login_user(created_user)
         created_user_dict = model_to_dict(created_user)
         created_user_dict.pop('password')
         return jsonify(data=created_user_dict, message=f'succesffully created user {created_user.username}', status=200), 200
+
+
+
+@users.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+
+    try:
+        user = models.User.get(models.User.email == payload['email'])
+        user_dict = model_to_dict(user)
+        user_dict.pop('password')
+        password_good = check_password_hash(user.password, payload['password'])
+        if password_good:
+            login_user(user)
+            return jsonify(data=user_dict, message='sucessfully logged in user', status=200), 200
+        else:
+            return jsonify(data={}, message='email or password incorrect', status=200), 200
+
+
+    except models.DoesNotExist:
+        return jsonify(data={}, message='email does not exist', status=200), 200
+
+
+@users.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return jsonify(data={}, message='successfully logged out user', status=200), 200
